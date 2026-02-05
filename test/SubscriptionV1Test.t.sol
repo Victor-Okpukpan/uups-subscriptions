c427b4159d718ca9186fa50d5e8a5b66c8e6fad9
Victor-Okpukpan <vokpukpan@gmail.com>
2026-02-05 22:14:50 +0100

    feat: finish SubscriptionV1 tests

--- test/SubscriptionV1Test.t.sol
+++ test/SubscriptionV1Test.t.sol
@@ -5,7 +5,10 @@ import {Test} from "forge-std/Test.sol";
 import {DeploySubscriptionV1} from "../script/DeploySubscriptionV1.s.sol";
 import {UpgradeSubscriptionV1} from "../script/UpgradeSubscriptionV1.s.sol";
 import {SubscriptionV1} from "../src/SubscriptionV1.sol";
+import {SubscriptionV2} from "../src/SubscriptionV2.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SubscriptionV1Test is Test {
     DeploySubscriptionV1 deployer;
@@ -18,6 +21,8 @@ contract SubscriptionV1Test is Test {
     address treasury;
     address user = makeAddr("user");
 
+    uint256 constant BILLING_PERIOD = 30 days;
+
     function setUp() public {
         deployer = new DeploySubscriptionV1();
         upgrader = new UpgradeSubscriptionV1();
@@ -26,6 +31,8 @@ contract SubscriptionV1Test is Test {
         config = _config;
 
         (usdc, treasury,, owner) = config.activeNetworkConfig();
+
+        deal(usdc, user, 500 * 1e6);
     }
 
     function test_subscriptionHasBeenDeployedAndInitialized() public {
@@ -139,11 +146,225 @@ contract SubscriptionV1Test is Test {
         vm.stopPrank();
     }
 
+    function test_RevertWhenSetTreasuryIsZeroAddress() public {
+        vm.startPrank(owner);
+        vm.expectRevert(SubscriptionV1.Subscription__ZeroAddress.selector);
+        proxy.setTreasury(address(0));
+        vm.stopPrank();
+    }
+
     function test_setTreasuryWorks() public {
         vm.startPrank(owner);
         proxy.setTreasury(address(0x2));
         vm.stopPrank();
 
         assertEq(proxy.s_treasury(), address(0x2));
+    }
+
+    function test_RevertWhenSubscribeToNonExistentPlan() public {
+        vm.startPrank(user);
+        vm.expectRevert(SubscriptionV1.Subscription__PlanDoesNotExist.selector);
+        proxy.subscribe(999); // Non-existent plan ID
+        vm.stopPrank();
+    }
+
+    function test_RevertWhenSubscribeToInactivePlan() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        proxy.setPlanStatus(planId, false); // Deactivate the plan
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        vm.expectRevert(SubscriptionV1.Subscription__PlanDoesNotExist.selector);
+        proxy.subscribe(planId);
+        vm.stopPrank();
+    }
+
+    function test_RevertWhenAlreadySubscribed() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        vm.expectRevert(SubscriptionV1.Subscription__AlreadySubscribed.selector);
+        proxy.subscribe(planId);
+        vm.stopPrank();
+    }
+
+    function test_SubscribeWorks() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        vm.stopPrank();
+
+        SubscriptionV1.Subscription memory subscription = proxy.getSubscription(user);
+        assertEq(subscription.planId, planId);
+        assertEq(subscription.active, true);
+        assert(subscription.nextPaymentDue > block.timestamp);
+    }
+
+    function test_RevertWhenRenewWithoutSubscription() public {
+        vm.startPrank(user);
+        vm.expectRevert(SubscriptionV1.Subscription__NotSubscribedToAnyPlan.selector);
+        proxy.renewSubscription();
+        vm.stopPrank();
+    }
+
+    function test_RevertWhenRenewBeforeExpiration() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        vm.expectRevert(SubscriptionV1.Subscription__NotYetExpired.selector);
+        proxy.renewSubscription();
+        vm.stopPrank();
+    }
+
+    function test_RenewSubscriptionWorks() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        skip(BILLING_PERIOD + 1);
+        proxy.renewSubscription();
+        vm.stopPrank();
+
+        SubscriptionV1.Subscription memory subscription = proxy.getSubscription(user);
+        assert(subscription.nextPaymentDue > block.timestamp);
+    }
+
+    function test_RevertWhenCancelWithoutSubscription() public {
+        vm.startPrank(user);
+        vm.expectRevert(SubscriptionV1.Subscription__NotSubscribedToAnyPlan.selector);
+        proxy.cancelSubscription();
+        vm.stopPrank();
+    }
+
+    function test_CancelSubscriptionWorks() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        proxy.cancelSubscription();
+        vm.stopPrank();
+
+        SubscriptionV1.Subscription memory subscription = proxy.getSubscription(user);
+        assertEq(subscription.active, false);
+        assertEq(subscription.planId, 0);
+        assertEq(subscription.nextPaymentDue, 0);
+    }
+
+    function test_RevertWhenGetNonExistentPlan() public {
+        vm.startPrank(user);
+        vm.expectRevert(SubscriptionV1.Subscription__PlanDoesNotExist.selector);
+        proxy.getPlan(999); // Non-existent plan ID
+        vm.stopPrank();
+    }
+
+    function test_GetPlanWorks() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        SubscriptionV1.Plan memory plan = proxy.getPlan(planId);
+        assertEq(plan.pricePerPeriod, 50);
+        assertEq(plan.active, true);
+    }
+
+    function test_GetSubscriptionWorks() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        vm.stopPrank();
+
+        SubscriptionV1.Subscription memory subscription = proxy.getSubscription(user);
+        assertEq(subscription.planId, planId);
+        assertEq(subscription.active, true);
+        assert(subscription.nextPaymentDue > block.timestamp);
+    }
+
+    function test_GetSubscriptionForNonSubscriber() public {
+        SubscriptionV1.Subscription memory subscription = proxy.getSubscription(user);
+        assertEq(subscription.planId, 0);
+        assertEq(subscription.active, false);
+        assertEq(subscription.nextPaymentDue, 0);
+    }
+
+    function test_IsPaymentDueReturnsFalseForNonSubscriber() public {
+        bool paymentDue = proxy.isPaymentDue(user);
+        assertEq(paymentDue, false);
+    }
+
+    function test_IsPaymentDueReturnsFalseForActiveSubscription() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        bool paymentDue = proxy.isPaymentDue(user);
+        vm.stopPrank();
+
+        assertEq(paymentDue, false);
+    }
+
+    function test_IsPaymentDueReturnsTrueWhenExpired() public {
+        vm.startPrank(owner);
+        uint256 planId = proxy.createPlan(50);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        IERC20(usdc).approve(address(proxy), type(uint256).max);
+        proxy.subscribe(planId);
+        skip(BILLING_PERIOD + 1);
+        bool paymentDue = proxy.isPaymentDue(user);
+        vm.stopPrank();
+
+        assertEq(paymentDue, true);
+    }
+
+    function test_UpgradeToSubscriptionV2() public {
+        vm.startPrank(owner);
+        SubscriptionV2 newImplementation = new SubscriptionV2();
+
+        address mockPriceFeed = makeAddr("mockPriceFeed");
+        bytes memory data = abi.encodeWithSelector(SubscriptionV2.initializeV2.selector, mockPriceFeed);
+        proxy.upgradeToAndCall(address(newImplementation), data);
+        SubscriptionV2 upgradedProxy = SubscriptionV2(address(proxy));
+
+        assertEq(upgradedProxy.s_ethUsdPriceFeed(), mockPriceFeed);
+        vm.stopPrank();
+    }
+
+    function test_UpgradeFailsWhenCalledByNonOwner() public {
+        vm.startPrank(user);
+        SubscriptionV2 newImplementation = new SubscriptionV2();
+
+        address mockPriceFeed = makeAddr("mockPriceFeed");
+        bytes memory data = abi.encodeWithSelector(SubscriptionV2.initializeV2.selector, mockPriceFeed);
+        vm.expectRevert();
+        proxy.upgradeToAndCall(address(newImplementation), data);
+        vm.stopPrank();
     }
 }
